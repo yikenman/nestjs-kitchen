d0b1b527c250cce332d8bfaaa3c7b64023a28707
yikenman <17548672+yikenman@users.noreply.github.com>
2025-11-09 23:08:00 +0800

    docs(changeset): fix type error (#64)

--- packages/mybatis-mapper/src/types.ts
+++ packages/mybatis-mapper/src/types.ts
@@ -1,7 +1,8 @@
 import type { Format } from 'mybatis-mapper';
 import type { GlobOptions } from 'tinyglobby';
 
-export type MybatisMapperModuleOptions = Omit<GlobOptions, 'onlyDirectories' | 'onlyFiles'> & {
+export type MybatisMapperModuleOptions = Omit<GlobOptions, 'onlyDirectories' | 'onlyFiles' | 'cwd'> & {
+  cwd?: string;
   format?: Format;
   watchPatterns?: GlobOptions['patterns'];
 };
